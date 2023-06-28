14d01abc7f6ff4b8556d7fb2eee42728604884db
divyakrishnan15 <divyabalajee@gmail.com>
2023-06-28 13:43:19 -0400

    update

--- src/components/Header/header.component.js
+++ src/components/Header/header.component.js
@@ -21,6 +21,20 @@ export default function Header() {
         <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg>
       
         </a>
+
+
+        {/* <!-- Google tag (gtag.js) --> */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-R0RC3VE7WP"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date());
+
+        gtag('config', 'G-R0RC3VE7WP');
+      </script>
+
+
+
     </div>
   );
 }
